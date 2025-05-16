d642359345a0da7c949cbed4f1604eb3fb194e98
yathu1 <yathu2198@gmail.com>
2025-05-16 18:05:03 +0530

    fix errors

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ export default withSentryConfig(nextConfig, {
 
 // Suppresses source map uploading logs during build
 silent: true,
-org: "yathuraj",
+org: "javascript-mastery",
 project: "javascript-nextjs",
 }, {
 // For all available options, see:
